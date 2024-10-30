750c2910c8fb11ab6d2b57316ee3d88883200550
wenipsptsr <wenipuspitasari029@gmail.com>
2024-10-30 11:54:56 +0700

    selesai

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 function main() {
   
+let hasilDadu = [1, 5, 2, 6, 2, 3];
+
+
+let kemunculanGanjil = 0;
+let kemunculanGenap = 0;
+
+
+for (let i = 0; i < hasilDadu.length; i++) {
+  if (hasilDadu[i] % 2 !== 0) {
+    kemunculanGanjil++; 
+  } else {
+    kemunculanGenap++; 
+  }
+}
+
+
+console.log(`Raka mendapatkan angka ganjil sebanyak ${kemunculanGanjil} kali dan angka genap sebanyak ${kemunculanGenap} kali`);
 
 
 }
